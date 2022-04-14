9e127c8af087d85237694e7a4c41f24e7879867d
vishal <vrana14693@gmail.com>
2022-04-14 19:37:00 +0530

    shuffle cards function done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+const cardImg = [
+  { src: "../public/img/helmet-1.png" },
+  { src: "../public/img/potion-1.png" },
+  { src: "../public/img/ring-1.png" },
+  { src: "../public/img/scroll-1.png" },
+  { src: "../public/img/shield-1.png" },
+  { src: "../public/img/sword-1.png" },
+];
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  // shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImg, ...cardImg]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({
+        ...card,
+        id: Math.random(),
+      }));
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+  console.log(cards, turns);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Magic App</h1>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
