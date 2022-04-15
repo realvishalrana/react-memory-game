bdc0ec0af0f5fe41a05c7c6e53436ffdfb661915
vishal <vrana14693@gmail.com>
2022-04-15 11:57:16 +0530

    comparing card logic done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import SingleCard from "./components/SingleCard";
 const cardImg = [
-  { src: "../public/img/helmet-1.png" },
-  { src: "../public/img/potion-1.png" },
-  { src: "../public/img/ring-1.png" },
-  { src: "../public/img/scroll-1.png" },
-  { src: "../public/img/shield-1.png" },
-  { src: "../public/img/sword-1.png" },
+  { src: "/img/helmet-1.png" },
+  { src: "/img/potion-1.png" },
+  { src: "/img/ring-1.png" },
+  { src: "/img/scroll-1.png" },
+  { src: "/img/shield-1.png" },
+  { src: "/img/sword-1.png" },
 ];
 
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   // shuffle cards
   const shuffleCards = () => {
@@ -24,12 +27,42 @@ function App() {
     setCards(shuffledCards);
     setTurns(0);
   };
-  console.log(cards, turns);
+
+  //handle choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  // compare 2 selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        console.log("those card match");
+        return resetTurn();
+      } else {
+        console.log("those card do not match");
+        return resetTurn();
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  //reset choice & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+  };
 
   return (
     <div className="App">
       <h1>Magic App</h1>
       <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map((card) => (
+          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/SingleCard.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./SingleCard.css";
+
+const SingleCard = ({ card, handleChoice }) => {
+  const handleClick = () => {
+    handleChoice(card);
+  };
+
+  return (
+    <div className="card">
+      <div>
+        <img className="front" src={card.src} alt="card front" />
+        <img
+          className="back"
+          src="/img/cover.png"
+          alt="cover"
+          onClick={handleClick}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SingleCard;
